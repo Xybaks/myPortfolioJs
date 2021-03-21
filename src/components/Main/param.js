ce1a63bc60d9e27a52cf5beb99b8edf69220a1fc
Xybaks <nikolai.berestevich@gmail.com>
2021-03-21 22:48:27 +0300

    Link to linkedIn was updated

--- src/components/Main/param.js
+++ src/components/Main/param.js
@@ -20,45 +20,48 @@ const param = {
                 "circle"
             ],
             "image": [
+
                 {
-                    "src": "https://png2.cleanpng.com/sh/25eeb05fe69425a97f13b42b48668d43/L0KzQYm3VsE3N5Nqf5H0aYP2gLBuTgJmbKZ9ReRuYXP3PbB3jb10fJJ5fZ92YX7kd7b0hf51NWZnSqZtZEHnc4K3VPQzNmM8Tqo7NUC2QYa5WcE1OmY6T6kEMEaxgLBu/kisspng-redux-react-npm-state-management-5b24dd1dc104d2.2768250315291425577906.png",
-                    "height": 23,
-                    "width": 23
-                },
-                {
-                    "src": "https://png2.cleanpng.com/sh/25eeb05fe69425a97f13b42b48668d43/L0KzQYm3VsE3N5Nqf5H0aYP2gLBuTgJmbKZ9ReRuYXP3PbB3jb10fJJ5fZ92YX7kd7b0hf51NWZnSqZtZEHnc4K3VPQzNmM8Tqo7NUC2QYa5WcE1OmY6T6kEMEaxgLBu/kisspng-redux-react-npm-state-management-5b24dd1dc104d2.2768250315291425577906.png",
-                    "height": 33,
-                    "width": 33
-                },
-                {
-                    "src": "https://png2.cleanpng.com/sh/5d7cac38e5a95243e05430d204319066/L0KzQYm3VMExN6F7fZH0aYP2gLBuTgJmaZR5RdxqdnH2c8PwkQQuepZ4iNH3c3n5dX7AhfIubJZ4gdl3LXfshLn8gr1idph6RdlydHj4cn68gfNkaZQ3TaJsZHG8RXA7VMUzQGM4UKMAMkO2RoOBVcMxPWM8RuJ3Zx==/kisspng-react-javascript-responsive-web-design-github-angu-github-5accac250cda95.4452823815233628530527.png",
+                    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/170px-React-icon.svg.png",
                     "height": 20,
                     "width": 20
                 },
                 {
-                    "src": "https://png2.cleanpng.com/sh/5d7cac38e5a95243e05430d204319066/L0KzQYm3VMExN6F7fZH0aYP2gLBuTgJmaZR5RdxqdnH2c8PwkQQuepZ4iNH3c3n5dX7AhfIubJZ4gdl3LXfshLn8gr1idph6RdlydHj4cn68gfNkaZQ3TaJsZHG8RXA7VMUzQGM4UKMAMkO2RoOBVcMxPWM8RuJ3Zx==/kisspng-react-javascript-responsive-web-design-github-angu-github-5accac250cda95.4452823815233628530527.png",
+                    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Redux_Logo.png/330px-Redux_Logo.png",
+                    "height": 30,
+                    "width": 20
+                },
+                {
+                    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/120px-HTML5_logo_and_wordmark.svg.png",
                     "height": 20,
                     "width": 20
                 },
                 {
-                    "src": "https://png2.cleanpng.com/sh/b1cabe5be45baed15b682df1923c076c/L0KzQYm3VcA0N51xiZH0aYP2gLBuTgdmal5pfehubHBzfbb1lL1pfJ5xRdV8c0OwhLnsTf9paZ9mRdV4ZHWwfLFuj70za6FmiNd7LYD1f7vsgCRqd58yi9pqZHXnb4K9VsA6O2gygOZ2bD3ngrF3hP94dl5vi58AYXXldIa9U8RjamY1SZC5M0S3RYGAU8E2OmY5SKc5Mke2QYG4TwBvbz==/kisspng-web-development-html-css3-the-ohana-code-logo-2cpaper-projection-shaded_1660937-html-dropdown-js-5aebd5634bb501.0344507315254050273101.png",
-                    "height": 20,
-                    "width": 20
-                },
-                {
-                    "src": "https://png2.cleanpng.com/sh/b1cabe5be45baed15b682df1923c076c/L0KzQYm3VcA0N51xiZH0aYP2gLBuTgdmal5pfehubHBzfbb1lL1pfJ5xRdV8c0OwhLnsTf9paZ9mRdV4ZHWwfLFuj70za6FmiNd7LYD1f7vsgCRqd58yi9pqZHXnb4K9VsA6O2gygOZ2bD3ngrF3hP94dl5vi58AYXXldIa9U8RjamY1SZC5M0S3RYGAU8E2OmY5SKc5Mke2QYG4TwBvbz==/kisspng-web-development-html-css3-the-ohana-code-logo-2cpaper-projection-shaded_1660937-html-dropdown-js-5aebd5634bb501.0344507315254050273101.png",
+                    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/85px-CSS3_logo_and_wordmark.svg.png",
                     "height": 30,
                     "width": 30
                 },
                 {
-                    "src": "https://png2.cleanpng.com/sh/62e092d5c4d897f8781ea29a0f94d1ad/L0KzQYm3U8I3N6Roj5H0aYP2gLBuTgJmbKZ9ReRuYXP3PbvolvF0a6NuiOY2YX7qhb3okr1kaaRoedZybnewg8XCjPUue5lqRdlydHj4cn68gfI5aWRqfdM9ZEe1R3AAWcA6QGQ1TaMAMkKzRYG3U8A3P2Y3RuJ3Zx==/kisspng-redux-react-javascript-angular-cascading-style-she-github-5ab8a3eea4d727.7909830515220500306752.png",
+                    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/170px-React-icon.svg.png",
                     "height": 20,
                     "width": 20
-                }, {
-                    "src": "https://png2.cleanpng.com/sh/62e092d5c4d897f8781ea29a0f94d1ad/L0KzQYm3U8I3N6Roj5H0aYP2gLBuTgJmbKZ9ReRuYXP3PbvolvF0a6NuiOY2YX7qhb3okr1kaaRoedZybnewg8XCjPUue5lqRdlydHj4cn68gfI5aWRqfdM9ZEe1R3AAWcA6QGQ1TaMAMkKzRYG3U8A3P2Y3RuJ3Zx==/kisspng-redux-react-javascript-angular-cascading-style-she-github-5ab8a3eea4d727.7909830515220500306752.png",
+                },
+                {
+                    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Redux_Logo.png/330px-Redux_Logo.png",
+                    "height": 30,
+                    "width": 20
+                },
+                {
+                    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/120px-HTML5_logo_and_wordmark.svg.png",
                     "height": 30,
                     "width": 30
-                }
+                },
+                {
+                    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/85px-CSS3_logo_and_wordmark.svg.png",
+                    "height": 20,
+                    "width": 20
+                },
+
             ]
         },
         "color": {
